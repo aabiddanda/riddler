6eab5b134ca54ea26299e39eda30710591565860
Arjun Biddanda <aab227@cornell.edu>
2016-10-10 10:43:35 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+riddler
+-------
+
+Attempts at solving the riddler puzzles from fivethirtyeight
+and other similar puzzles (typically statistical or whimsical in nature)
---
+++ src/count.py
@@ -0,0 +1,52 @@
+#!/usr/local/bin/python3
+
+'''
+	Script to solve counting puzzle for Count Von Count on Twitter
+'''
+
+import argparse as arg
+from num2words import num2words
+
+'''
+	Returns character string of the count
+'''
+def count(n):
+	num_str = num2words(n) + '!'
+	num_str = num_str.replace(' and ', " ")
+	num_str = num_str.replace('-', ' ')
+	num_str = num_str.replace(',', ' ')
+	return(num_str)
+
+
+'''
+	Returns the first number that breaks the character bound c:
+	@args c - number of characters allowed
+	@return  - 
+'''
+def max_count(c):
+	max_cnt = 0
+	n = 0
+	place = 0
+	while max_cnt < c:
+		i = [n + int(x*(10**place)) for x in range(1,10)]
+		l = [len(count(a)) for a in i]
+		m = max(l)
+		max_cnt = m
+		for ni in range(0,len(i)):
+			if (l[ni] >= c):
+				n = i[ni]
+				print("Limit : %d\tlen : %d" % (i[ni], l[ni]))
+				break
+			if l[ni] == m:
+				n = i[ni]
+				break
+		place += 1
+	return(n) 
+
+if __name__ =='__main__':
+	parser = arg.ArgumentParser()
+	parser.add_argument('-c', required=True, type=int, default=20, help='character limit')
+	args = parser.parse_args()
+
+	# Step 1: Apply recursive equation to find where the maximum is greatest
+	max_count(args.c)	        
